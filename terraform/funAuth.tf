edc45755f121474e0bd9a1700bc4cc07d97ac0ca
Mike Wallick <mike@wallick.net>
2022-05-26 14:20:12 -0500

    Adds authenticator and enrollment policies

--- terraform/funAuth.tf
+++ terraform/funAuth.tf
@@ -23,13 +23,115 @@ resource "okta_group" "funauth_users" {
   name        = "FunAuth.Users"
   description = "Users in this group are regular FunAuth users"
 }
+resource okta_authenticator "okta_email" {
+  key      = "okta_email"
+  name     = "Email"
+  status   = "ACTIVE"
+  settings = jsonencode(
+  {
+    "allowedFor": "any",
+    "tokenLifetimeInMinutes": 5
+  }
+  )
+}
+resource okta_authenticator "okta_verify" {
+  key      = "okta_verify"
+  name     = "Okta Verify"
+  status   = "ACTIVE"
+  settings = jsonencode(
+  {
+    "compliance": {
+      "fips": "OPTIONAL"
+      },
+      "channelBinding": {
+          "style": "NUMBER_CHALLENGE",
+          "required": "HIGH_RISK_ONLY"
+      },
+      "userVerification": "PREFERRED"
+  }
+  )
+}
+resource okta_authenticator "phone_number" {
+  key      = "phone_number"
+  name     = "Phone"
+  status   = "ACTIVE"
+  settings = jsonencode(
+  {
+    "allowedFor": "any"
+  }
+  )
+}
+resource okta_authenticator "webauthn" {
+  key    = "webauthn"
+  name   = "Security Key or Biometric"
+  status = "ACTIVE"
+}
+resource "okta_policy_mfa" "funauth_admin_policy" {
+  name        = "FunAuth Admins"
+  status      = "ACTIVE"
+  description = "Allowed Authenticators for FunAuth admins"
+  is_oie      = true
+  priority    = 1
+  
+  okta_email = {
+    enroll = "REQUIRED"
+  }
+  okta_verify = {
+    enroll = "NOT_ALLOWED"
+  }
+  okta_password = {
+    enroll = "REQUIRED"
+  }
+  phone_number = {
+    enroll = "OPTIONAL"
+  }
+  webauthn = {
+    enroll = "NOT_ALLOWED"
+  }
+
+  groups_included = ["${okta_group.funauth_admins.id}"]
+}
+resource okta_policy_rule_mfa "funauth_admin_policy_rule" {
+  name      = "Default rule"
+  policy_id = okta_policy_mfa.funauth_admin_policy.id
+  status    = "ACTIVE"
+}
+resource "okta_policy_mfa" "funauth_user_policy" {
+  name        = "FunAuth Users"
+  status      = "ACTIVE"
+  description = "Allowed Authenticators for FunAuth users"
+  is_oie      = true
+  priority    = 2
+  
+  okta_email = {
+    enroll = "REQUIRED"
+  }
+  okta_verify = {
+    enroll = "NOT_ALLOWED"
+  }
+  okta_password = {
+    enroll = "REQUIRED"
+  }
+  phone_number = {
+    enroll = "OPTIONAL"
+  }
+  webauthn = {
+    enroll = "NOT_ALLOWED"
+  }
+
+  groups_included = ["${okta_group.funauth_users.id}"]
+}
+resource okta_policy_rule_mfa "funauth_user_policy_rule" {
+  name      = "Default rule"
+  policy_id = okta_policy_mfa.funauth_user_policy.id
+  status    = "ACTIVE"
+}
 resource "okta_app_oauth" "funauth_oidc" {
-  label       = "FunAuth Lab"
-  type        = "browser"
+  label          = "FunAuth Lab"
+  type           = "browser"
   response_types = ["code"]
   issuer_mode    = "CUSTOM_URL"
-  token_endpoint_auth_method = "none" # enables PKCE
-  grant_types = [
+  grant_types    = [
       "authorization_code",
       "interaction_code",
       "refresh_token"
@@ -43,16 +145,17 @@ resource "okta_app_oauth" "funauth_oidc" {
   lifecycle {
      ignore_changes = [groups]
   }
+  token_endpoint_auth_method = "none" # enables PKCE
 }
 # create the custom field for app role
 resource "okta_app_user_schema_property" "funauth_app_role" {
-  app_id = okta_app_oauth.funauth_oidc.id
-  index = "funAuthRole"
-  title = "FunAuth Role"
-  type = "string"
+  app_id      = okta_app_oauth.funauth_oidc.id
+  index       = "funAuthRole"
+  title       = "FunAuth Role"
+  type        = "string"
   description = "Pick list for funAuth app role"
-  master = "OKTA"
-  scope = "NONE"
+  master      = "OKTA"
+  scope       = "NONE"
   permissions = "READ_WRITE"
   enum = [
     "ADMIN",
@@ -69,9 +172,9 @@ resource "okta_app_user_schema_property" "funauth_app_role" {
 }
 # Create the App Assignment
 resource "okta_app_group_assignment" "admin" {
-  app_id   = okta_app_oauth.funauth_oidc.id
-  group_id = okta_group.funauth_admins.id
-  priority = 1
+  app_id     = okta_app_oauth.funauth_oidc.id
+  group_id   = okta_group.funauth_admins.id
+  priority   = 1
   depends_on = [
     okta_app_user_schema_property.funauth_app_role
   ]
@@ -82,9 +185,9 @@ resource "okta_app_group_assignment" "admin" {
   )
 }
 resource "okta_app_group_assignment" "users" {
-  app_id   = okta_app_oauth.funauth_oidc.id
-  group_id = okta_group.funauth_users.id
-  priority = 2
+  app_id     = okta_app_oauth.funauth_oidc.id
+  group_id   = okta_group.funauth_users.id
+  priority   = 2
   depends_on = [
     okta_app_user_schema_property.funauth_app_role
   ]
@@ -100,25 +203,24 @@ resource "okta_trusted_origin" "funauth_http" {
   scopes = ["REDIRECT", "CORS"]
 }
 resource "okta_inline_hook" "token-hook" {
-  name    = "Token Example"
+  name    = "Token Hook API-Example 1"
   version = "1.0.0"
   type    = "com.okta.oauth2.tokens.transform"
   channel = {
     version = "1.0.0"
-    uri     = "https://g4pywy3jvd.execute-api.us-east-2.amazonaws.com/token_hook"
+    uri     = "https://api.thorax.studio/hooks/oidc/example-1"
     method  = "POST"
   }
-  # auth = {
-  #   key   = "Authorization"
-  #   type  = "HEADER"
-  #   value = "secret"
-  # }
+  auth = {
+    key   = "x-api-key"
+    type  = "HEADER"
+    value = "z8h7m92m4s8ancCSK5tuJ1DODuE7DvOl6MNIeDoB"
+  }
 }
 resource "okta_auth_server" "funauth_authz" {
   name        = "FunAuth"
   description = "Generated by Terraform"
   audiences   = ["api://funAuth"]
-  credentials_rotation_mode = "AUTO"
   issuer_mode = "CUSTOM_URL"
 }
 resource "okta_auth_server_policy" "funauth_policy" {
@@ -130,12 +232,11 @@ resource "okta_auth_server_policy" "funauth_policy" {
   client_whitelist = ["${okta_app_oauth.funauth_oidc.id}"]
 }
 resource "okta_auth_server_policy_rule" "funauth_policy_rule" {
-  auth_server_id       = okta_auth_server.funauth_authz.id
-  policy_id            = okta_auth_server_policy.funauth_policy.id
-  status               = "ACTIVE"
-  name                 = "one_hour"
-  priority             = 1
-  #group_whitelist      = ["${data.okta_group.all.id}"]
+  auth_server_id  = okta_auth_server.funauth_authz.id
+  policy_id       = okta_auth_server_policy.funauth_policy.id
+  status          = "ACTIVE"
+  name            = "one_hour"
+  priority        = 1
   group_whitelist = [
     okta_group.funauth_admins.id,
     okta_group.funauth_users.id,
@@ -146,27 +247,27 @@ resource "okta_auth_server_policy_rule" "funauth_policy_rule" {
 }
 resource okta_auth_server_claim "approle" {
   auth_server_id = okta_auth_server.funauth_authz.id
-  name = "funAuthRole"
-  value = "appuser.funAuthRole"
-  claim_type = "IDENTITY"
+  name           = "funAuthRole"
+  value          = "appuser.funAuthRole"
+  claim_type     = "IDENTITY"
   always_include_in_token = true
 }
 resource "okta_auth_server_claim" "groups" {
   auth_server_id = okta_auth_server.funauth_authz.id
-  name = "groups"
-  value_type = "GROUPS"
+  name           = "groups"
+  value_type     = "GROUPS"
+  value          = "FunAuth."
+  claim_type     = "IDENTITY"
   group_filter_type = "STARTS_WITH"
-  value = "FunAuth."
-  claim_type = "IDENTITY"
   always_include_in_token = true
 }
 resource "okta_auth_server_claim" "usergroup" {
   auth_server_id = okta_auth_server.funauth_authz.id
-  name = "userGroup"
-  value_type = "GROUPS"
+  name           = "userGroup"
+  value_type     = "GROUPS"
+  value          = ".Users"
+  claim_type     = "IDENTITY"
   group_filter_type = "CONTAINS"
-  value = ".Users"
-  claim_type = "IDENTITY"
   always_include_in_token = true
 }
 #
